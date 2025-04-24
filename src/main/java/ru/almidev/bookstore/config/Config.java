8412314b9f01d4db9616c2e846176cf313d9e01c
Alexander Mikhailov <aomikhailov@mail.ru>
2025-04-24 20:13:07 +0700

    feat: добавлен .env.example.

---
+++ src/main/java/ru/almidev/bookstore/config/Config.java
@@ -0,0 +1,38 @@
+package ru.almidev.bookstore.config;
+import io.github.cdimascio.dotenv.Dotenv;
+
+/**
+ * Класс для работы с переменными конфигурации, загружаемыми из файла .env.
+ * Предоставляет методы для получения значений ключевых параметров приложения.
+ */
+public class Config {
+    private static final Dotenv dotenv;
+
+    static {
+        try {
+            dotenv = Dotenv.configure().load();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка инициализации переменных окружения из файла .env", e);
+        }
+    }
+
+    public static String getAppName() {
+        return dotenv.get("APP_NAME");
+    }
+
+    public static String getDbUrl() {
+        return dotenv.get("DB_URL");
+    }
+
+    public static String getDbUser() {
+        return dotenv.get("DB_USER");
+    }
+
+    public static String getDbPassword() {
+        return dotenv.get("DB_PASSWORD");
+    }
+
+    public static String getViewPath() {
+        return dotenv.get("VIEW_PATH");
+    }
+}
